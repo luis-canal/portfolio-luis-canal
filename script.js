757930a2a22a92a33d52629561a7d48d35868407
Luis E. B. Canal <luiseduardocanal@gmail.com>
2026-04-17 13:55:03 -0300

    adicionado navbar inteligente - efeito ao rolar

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const elementos = document.querySelectorAll("section");
 
 const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    }
-  });
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
 });
 
 elementos.forEach(el => observer.observe(el));
+
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
